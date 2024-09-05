459748aec56d54c4a31f080333c331c069c1b661
PL1106 <pleon0419@gmail.com>
2024-09-05 12:41:01 +0100

    created search bar and setup files
    relates #1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,11 +3,10 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <h1>Microblogging Site</h1>
+      <h1>Micro-blogging Site</h1>
       <input type="text" placeholder="Search..." className="search-input" />
     </header>
   );
 }
 
 export default Header;
-
