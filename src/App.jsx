2eefefdd1d214b7d2a4c48e3a223b242a47d3138
PL1106 <pleon0419@gmail.com>
2024-09-09 11:45:38 +0100

    I added files for the comment section
    relates #13

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import Header from './components/Header'
-import './style/header.css'
-import PostButton from './components/Postbutton';
-import './style/post-button.css'
-
-
+import React from 'react';
+import Header from './components/Header';
+import Comment from './components/Comment';
+import './style/header.css';
+import './style/post-button.css';
+import './style/comment.css';
 
 function App() {
-  const handlePostClick = () => {
-    console.log('Post button clicked');
-  };
-
   return (
     <div>
       <Header />
-      <div style={{ padding: '20px', textAlign: 'center' }}>
-        <PostButton onClick={handlePostClick} label="Post" />
+      <div style={{ padding: '20px', display: 'flex', justifyContent: 'center' }}>
+        <Comment />
       </div>
     </div>
   );
