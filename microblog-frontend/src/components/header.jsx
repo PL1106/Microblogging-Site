6e57ebc8a25eb9b5fe4d8aea007dec15dc794b91
mb556 <moamiin54@outlook.com>
2024-09-09 11:34:40 +0100

    fixed the routes of the components and css
    relates #14

---
+++ microblog-frontend/src/components/header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Micro-blogging Site</h1>
+      <input type="text" placeholder="Search..." className="search-input" />
+    </header>
+  );
+}
+
